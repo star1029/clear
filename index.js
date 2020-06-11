53abb318680b23804f0b5d822408f527adfc354d
小生有礼 <2321582517@qq.com>
2020-06-11 23:41:42 +0800

    点击空白区域连带周边

--- index.js
+++ index.js
@@ -44,13 +44,60 @@ let setNode = (number)=> {
 		};
 		x_node.style.width = node_width;
 		x_node.style.height = node_height;
+		x_node.style.lineHeight = node_height;
+		x_node.dataset.index = i;
 		x_main.appendChild(x_node);
 	};
 	// 点击事件
 	x_main.addEventListener('click', e=>{
 		if(e.target.nodeName.toLowerCase() === 'span'){
 			if(!e.target.classList.contains('ray_node')){
-				
+				let index = Number(e.target.dataset.index),
+						list = x_main.getElementsByTagName("span"),
+						arr = [],
+						num = 0;
+				e.target.className = "contain_node flip";
+				//左上
+				if(index == 0){
+					arr = [1, x_around_num, x_around_num+1];
+				};
+				//右上
+				if(index == x_around_num - 1){
+					arr = [index - 1, index - 1 + x_around_num, x_around_num*2 - 1];
+				};
+				//左下
+				if(index == x_total - x_around_num){
+					arr = [index - x_around_num, index - x_around_num + 1, x_total - x_around_num + 1];
+				};
+				//右下
+				if(index == x_total - 1){
+					arr = [index - 1 - x_around_num,index - x_around_num,index - 1];
+				};
+				//上方
+				if(index > 0 && index < x_around_num){
+					arr = [index - 1, index - 1 + x_around_num, index + x_around_num, 1 + index + x_around_num , 1 + index];
+				};
+				//右方
+				if(index > x_around_num && index < x_total - 1 && (index + 1)%x_around_num == 0){
+					arr = [index - 1 - x_around_num, index - x_around_num, index - 1, index - 1 + x_around_num, index + x_around_num];
+				};
+				//下方
+				if(index > x_total - x_around_num && index < x_total - 1){
+					arr = [index - 1, index - 1 - x_around_num, index - x_around_num, index + 1 - x_around_num, index + 1];
+				};
+				//左方
+				if(index > 0 && index < x_total - x_around_num && index%x_around_num == 0){
+					arr = [index - x_around_num, index - x_around_num + 1, index + 1, index + x_around_num, index + x_around_num + 1];
+				};
+				arr.forEach(item=>{
+					console.log(list[item])
+					if(list[item].classList.contains('ray_node')){
+						num++;
+					}else{
+						list[item].className = "contain_node flip";
+					}
+				});
+				list[index].innerHTML = num == 0 ? '' : num;
 			}else{
 				console.log('失败了！');
 			};
@@ -63,9 +110,9 @@ setNode(x_num.innerHTML);
 // 切换扫雷难度
 x_select.onchange = () => {
 	let val = x_select.options[x_select.selectedIndex].value;
-	if(val == 'easy'){ x_total = 36; x_num.innerHTML = 6; }
-		else if(val == 'medium'){ x_total = 81; x_num.innerHTML = 9; }
-			else { x_total = 144; x_num.innerHTML = 12; }
+	if(val == 'easy'){ x_total = 36; x_around_num = Math.sqrt(x_total); x_num.innerHTML = 6; }
+		else if(val == 'medium'){ x_total = 81; x_around_num = Math.sqrt(x_total); x_num.innerHTML = 9; }
+			else { x_total = 144; x_around_num = Math.sqrt(x_total); x_num.innerHTML = 12; }
 	setNode(x_num.innerHTML);
 };
 // 计时
@@ -86,10 +133,12 @@ x_begin.onclick = () => {
 		x_begin.innerHTML = "开始";
 		timeEnd();
 		x_time.innerHTML = 0;
+		x_select.removeAttribute("disabled");
 	}else{
 		x_begin.className = "begin";
 		x_shade.className = "contain-shade hide";
 		x_begin.innerHTML = "结束此关";
 		timeActive();
+		x_select.setAttribute("disabled","disabled");
 	};
 };
